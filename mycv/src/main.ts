434d6324b4a08d46d96d6c1a015ca74bc00f0710
mushkan27 <muskan51.lama@gmail.com>
2026-08-06 14:20:48 +0545

    feat: add CRUD endpoints and lifecycle hooks for users

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
+      whitelist: true, //removes properties that are not in the dto
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,10 +1,51 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Delete,
+  Patch,
+  Param,
+  Query,
+  NotFoundException,
+  UseInterceptors, //to modify request and response
+  ClassSerializerInterceptor, //to transform class instances to JSON, based on some rules in an entity
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dtos';
+import { UpdateUserDto } from './dtos/update-user.dtos';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
-  @Post()
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    return this.usersService.create(body.email, body.password);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
